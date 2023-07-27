48ac5663a077dbace197e58aca95a072435dbed0
sandeep540 <kn.sandeep@gmail.com>
2023-07-27 14:44:35 +0530

    updated

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("hello world AI PR")
+
+	readFile("sample.txt")
+}
+
+func readFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("file --> ", file)
+
+	file.Close()
 }
